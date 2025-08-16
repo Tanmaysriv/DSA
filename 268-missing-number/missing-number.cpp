bdee145e550d02543850e37711dc76b00198a913
Tanmay Srivastava <srivastavatanmay199@gmail.com>
2025-08-16 14:10:10 +0530

    Time: 40 ms (5.07%) | Memory: 29.6 MB (5.04%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<=n;i++)
+        {
+            if(mp[i]==0)
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
