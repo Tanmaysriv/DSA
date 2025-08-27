e61484bdb440c30c5796da07e1dfbdd01e39479e
Tanmay Srivastava <srivastavatanmay199@gmail.com>
2025-08-27 22:42:59 +0530

    Time: 127 ms (91.98%) | Memory: 145.4 MB (29.95%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,47 +1,78 @@
 class Solution {
 public:
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int dirs[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-        int memo[m][n][4][2];
-        memset(memo, -1, sizeof(memo));
-
-        function<int(int, int, int, bool, int)> dfs =
-            [&](int cx, int cy, int direction, bool turn, int target) -> int {
-            int nx = cx + dirs[direction][0];
-            int ny = cy + dirs[direction][1];
-            /* If it goes beyond the boundary or the next node's value is not
-             * the target value, then return */
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
-                grid[nx][ny] != target) {
-                return 0;
+        const int n = size(grid);
+        const int m = size(grid[0]);
+        int result = 0;
+        vector<vector<int>> down_right(n, vector<int>(m, 1));
+        vector<vector<int>> down_left(n, vector<int>(m, 1));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                const int x = grid[i][j];
+                if (x == 1) {
+                    result = 1;
+                    continue;
+                }
+                if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] == 2 - x) {
+                    down_right[i][j] = down_right[i - 1][j - 1] + 1;
+                }
+                if (i - 1 >= 0 && j + 1 < m && grid[i - 1][j + 1] == 2 - x) {
+                    down_left[i][j] = down_left[i - 1][j + 1] + 1;
+                }
             }
-            if (memo[nx][ny][direction][turn] != -1) {
-                return memo[nx][ny][direction][turn];
+        }
+        vector<vector<int>> up_right(n, vector<int>(m, 1));
+        vector<vector<int>> up_left(n, vector<int>(m, 1));
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = 0; j < m; ++j) {
+                const int x = grid[i][j];
+                if (x == 1) {
+                    continue;
+                }
+                if (i + 1 < n && j - 1 >= 0 && grid[i + 1][j - 1] == 2 - x) {
+                    up_right[i][j] = up_right[i + 1][j - 1] + 1;
+                }
+                if (i + 1 < n && j + 1 < m && grid[i + 1][j + 1] == 2 - x) {
+                    up_left[i][j] = up_left[i + 1][j + 1] + 1;
+                }
             }
-
-            /* Continue walking in the original direction. */
-            int maxStep = dfs(nx, ny, direction, turn, 2 - target);
-            if (turn) {
-                /* Clockwise rotate 90 degrees turn */
-                maxStep = fmax(maxStep, dfs(nx, ny, (direction + 1) % 4, false,
-                                            2 - target));
-            }
-            memo[nx][ny][direction][turn] = maxStep + 1;
-            return maxStep + 1;
-        };
-
-        int res = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    for (int direction = 0; direction < 4; ++direction) {
-                        res = fmax(res, dfs(i, j, direction, true, 2) + 1);
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                const int x = grid[i][j];
+                if (x == 1) {
+                    continue;
+                }
+                if ((down_right[i][j] % 2 == 0 && x == 0) || (down_right[i][j] % 2 == 1 && x == 2)) {
+                    const int ni = i - down_right[i][j];
+                    const int nj = j - down_right[i][j];
+                    if (0 <= ni && ni < n && 0 <= nj && nj < m && grid[ni][nj] == 1) {
+                        result = max(result, down_right[i][j] + up_right[i][j]);  // >
+                    }
+                }
+                if ((down_left[i][j] % 2 == 0 && x == 0) || (down_left[i][j] % 2 == 1 && x == 2)) {
+                    const int ni = i - down_left[i][j];
+                    const int nj = j + down_left[i][j];
+                    if (0 <= ni && ni < n && 0 <= nj && nj < m && grid[ni][nj] == 1) {
+                        result = max(result, down_left[i][j] + down_right[i][j]);  // v
+                    }
+                }
+                if ((up_left[i][j] % 2 == 0 && x == 0) || (up_left[i][j] % 2 == 1 && x == 2)) {
+                    const int ni = i + up_left[i][j];
+                    const int nj = j + up_left[i][j];
+                    if (0 <= ni && ni < n && 0 <= nj && nj < m && grid[ni][nj] == 1) {
+                        result = max(result, up_left[i][j] + down_left[i][j]);  // <
+                    }
+                }
+                if ((up_right[i][j] % 2 == 0 && x == 0) || (up_right[i][j] % 2 == 1 && x == 2)) {
+                    const int ni = i + up_right[i][j];
+                    const int nj = j - up_right[i][j];
+                    if (0 <= ni && ni < n && 0 <= nj && nj < m && grid[ni][nj] == 1) {
+                        result = max(result, up_right[i][j] + up_left[i][j]);  // ^
                     }
                 }
             }
         }
-
-        return res;
+        return result;
     }
 };
