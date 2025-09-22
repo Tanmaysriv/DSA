ace41169318a5cc8ba09ccdf4e9cbd504805884b
Tanmay Srivastava <srivastavatanmay199@gmail.com>
2025-09-22 18:32:16 +0530

    Time: 15 ms (68.10%) | Memory: 35.7 MB (67.00%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        int n = grid.size();
+        map<vector<int>, int> rowMap; // map for row patterns
+        
+        // store each row
+        for (auto &row : grid) rowMap[row]++;
+
+        int ans = 0;
+        // check each column
+        for (int c = 0; c < n; c++) {
+            vector<int> col;
+            for (int r = 0; r < n; r++) col.push_back(grid[r][c]);
+            if (rowMap.count(col)) ans += rowMap[col];
+        }
+        return ans;
+    }
+};
